b89948958272cbd5c2cb9b69dc1b32847c978d99
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-12 14:17:18 +0530

    upload

---
+++ New Work File/Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<< "my name is Kavindu "<<endl;
+	cout<< "and i love samgsung "<<endl; 
+	
+	cout <<" my \n name \n is \n kavindu";
+	return 0;
+}
