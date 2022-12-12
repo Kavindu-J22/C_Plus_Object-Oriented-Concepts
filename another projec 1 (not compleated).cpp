5cdda13ee0535abfad19590bfe6f1d8614525a63
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-12 14:16:28 +0530

    upload

---
+++ another projec 1 (not compleated).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ 
+using namespace std;
+int main ()
+{
+string charname = "john";
+	
+	cout<< "there once was a man named John"<<endl;
+	cout<<"he was 35 years old"<<endl;
+	cout<<"He liked The name Jhone"<<endl;
+	cout<<"but did not like being 35";
+	
+	return 0;
+}
---
+++ proj 2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a,b,c,sum;
+
+	cout <<"\nInput your age :";
+	cin >> a ;
+	
+	cout <<"\ninput your think age :";
+	cin >> b;
+	
+	sum = a +  b;
+	
+	cout <<"\nyour age is = "<< a <<endl;
+	cout <<"your think age is = "<<b<<endl;
+	cout <<"your final age is = "<<sum<<endl;
+	return 0; 
+}// This is only athul projet.and This is not meaning.
