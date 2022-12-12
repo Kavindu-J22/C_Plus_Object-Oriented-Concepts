774a540bfd38a3255c61528a3fb88eaaa7c2f6be
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-12 14:21:49 +0530

    upload

---
+++ My Works/c++ 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  	int x,y = 6,z = 10;
+	
+  cout << "Hello World!" <<endl;
+  cout << "I am learning C++"<<endl;
+  
+
+	int sum = x + y;
+	cout << sum << endl;
+	cout << x+y+z;
+	
+	cout << "Type a number: "; // Type a number and press enter
+cin >> x; // Get user input from the keyboard
+cout << "Your number is: " << x; // Display the input value
+  return 0;
+}
+
